1db6fb90513b561faed9afaf86f56f1824517984
LearnAcademys <adefemiolowayomi@gmail.com>
2025-10-23 00:53:19 +0100

    Updated the app.py to teach and demonstrate git pull

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 WIP change
 Git
-
+edited this to reflect to rflect and teach git pull
 
 
 Git is a distributed version control system (DVCS) designed to handle everything from small to very large projects with speed and efficiency. It allows multiple developers to work on the same project without interfering with each other's changes. GitHub, now owned by Microsoft, is a popular platform for hosting Git repositories, enabling collaboration, code reviews, and integration with modern CI/CD pipelines.
